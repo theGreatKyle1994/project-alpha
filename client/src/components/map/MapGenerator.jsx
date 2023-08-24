1f479337e6d29485fcf1499b3b698baae3b2b8c7
Joel Jacobson <joel.e.jacobson@gmail.com>
2023-08-24 11:13:51 -0600

    working on spawning

--- client/src/components/map/MapGenerator.jsx
+++ client/src/components/map/MapGenerator.jsx
@@ -1,5 +1,6 @@
 // import generatePopulatedChunk from "../../utilities/map/functions/create-populated-chunk";
 import generateMap from "../../utilities/map/map-engine";
+import playerStartPoint from "../../utilities/map/functions/player-start-point";
 import Row from "./tiles/Row";
 import Tile from "./tiles/Tile";
 import TileWall from "./tiles/TileWall";
@@ -10,6 +11,8 @@ import "../../css/map/tiles.css";
 const MapGenerator = () => {
   // generate a map based on three different algos: web, chain, or spoke
   const randomMap = generateMap(2, 2);
+  const startingPoint = playerStartPoint(randomMap)
+  console.log(startingPoint)
   return (
     <>
       {randomMap.map((row, rIndex) => {
