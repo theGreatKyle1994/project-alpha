3316c2209ccd1f2bdea752e146568d7d76910c04
Kyle Rohr-Morey <kylemorey1994@gmail.com>
2023-09-30 19:16:54 -0400

    App cleanup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,11 @@
+import { createContext, useState } from "react";
 import { Routes, Route } from "react-router-dom";
 // Testing Environments
 import EntityTesting from "./components/testing/EntityTesting";
 import CanvasTesting from "./components/testing/CanvasTesting";
-import MapGenerator from "./components/map/MapGenerator";
 // Core Game
 import GameCore from "./components/core/GameCore";
 import Player from "./entities/player/Player";
-import { createContext, useState } from "react";
 // CSS Imports
 import "./App.css";
 
@@ -21,7 +20,6 @@ const App = () => {
       <Routes>
         <Route path="/entity" element={<EntityTesting />} />
         <Route path="/canvas" element={<CanvasTesting />} />
-        <Route path="/map" element={<MapGenerator />} />
         <Route path="/game" element={<GameCore />} />
       </Routes>
     </globalContext.Provider>
