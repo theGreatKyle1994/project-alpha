c8237648681d62028d4d0caca3af1307c12072cc
Kyle Rohr-Morey <kylemorey1994@gmail.com>
2023-10-28 13:11:21 -0400

    updated backend code to flow with frontend syntax

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,12 +4,14 @@ import EntityTesting from "./components/testing/EntityTesting";
 import CanvasTesting from "./components/testing/CanvasTesting";
 // Core Game
 import Engine from "./components/core/engine/Engine";
+import LoginCore from "./Backend/LoginCore";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/entity" element={<EntityTesting />} />
       <Route path="/canvas" element={<CanvasTesting />} />
+      <Route path="/login" element={<LoginCore />} />
       <Route path="/game" element={<Engine />} />
     </Routes>
   );
---
+++ client/src/components/backend/LoginCore.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+// Todo for player login/guest bypass
+const LoginCore = () => {
+  const navigate = useNavigate;
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <form></form>
+    </div>
+  );
+};
+
+export default LoginCore;
