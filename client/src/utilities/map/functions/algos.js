b50ebcb5bb0a47b155e4bd1ab5bacbc37feb60b5
Joel Jacobson <joel.e.jacobson@gmail.com>
2023-11-05 16:34:30 -0700

    adds inline comments documenting map engine

--- client/src/utilities/map/functions/algos.js
+++ client/src/utilities/map/functions/algos.js
@@ -2,13 +2,18 @@ import { applyRange } from "../../general/functions/utilityFunctions";
 import { createHorizontalPath, createVerticalPath } from "./create-paths";
 
 // Chain algo takes the randomly generated list of endpoints and links
-// the first to the second, the second to the third, and so on
+// the first to the second, the second to the third, and so on.
+
+// newChunk is the 2D array, and endPoints are the index coordinates of each 2 in newChunk
 export const createChain = ({ newChunk, endPoints }) => {
+  // iteration takes endpoint in a row (start), the endpoint in the next row (end)
+  // and changes the 0's between the two points to a 2.
   for (let i = 0; i < endPoints.length; i++) {
     const start = endPoints[i];
     const end = endPoints[(i + 1) % endPoints.length];
-    const rise = start.row - end.row;
-    const run = start.col - end.col;
+    const rise = start.row - end.row; // number of rows between start and end
+    const run = start.col - end.col; // number of cols between start and end
+    // flip coin to determine first moving vertically or horizontally
     const moveDirection = applyRange(0, 2);
     if (moveDirection) {
       createHorizontalPath(newChunk, rise, run, end, start);
@@ -18,17 +23,17 @@ export const createChain = ({ newChunk, endPoints }) => {
   }
   return newChunk;
 };
-// Spoke algo takes list of randomly generated endpoints, chooses one a
+// Spoke algo takes list of randomly generated endpoints, chooses one as
 // the hub, then connects that one hub to every other endpoint.
 export const createSpoke = ({ newChunk, endPoints }) => {
-  // select random endpoint to be the hub
+  // select random endpoint to be the hub. This hub is not necessarily in the center.
+  // It can be any one of the coords in endPoints.
   const hubIndex = applyRange(0, endPoints.length);
-  const hubTile = endPoints.splice(hubIndex, 1).pop();
+  const hubTile = endPoints.splice(hubIndex, 1).pop(); // random index in previous line is assigned to hubTile, then removed
   endPoints.forEach((spoke) => {
     // calculate the row and col distance between the hub and the spoke
     const rise = hubTile.row - spoke.row;
     const run = hubTile.col - spoke.col;
-    // function when moving vertically first
     // flip coin to determine first moving vertically or horizontally
     const movement = applyRange(0, 2);
     if (movement) {
@@ -37,20 +42,26 @@ export const createSpoke = ({ newChunk, endPoints }) => {
       createHorizontalPath(newChunk, rise, run, spoke, hubTile);
     }
   });
-
+  
   return newChunk;
 };
 // Web algo takes a list of randomly generated endpoints and connect each
 // endpoint to every other endpoint. Very open and blobby result.
-export const createWeb = ({ newChunk, endPoints }) => {
-  // randomize whether path creation should start vert or horiz
-  const movement = applyRange(0, 2);
 
+// Web algo is the most inefficient because it is likely iterating over 2's and rewriting them as 2's.
+export const createWeb = ({ newChunk, endPoints }) => {
+  // flip coin to determine first moving vertically or horizontally
+  const movement = applyRange(0, 2);
+  
   while (endPoints.length > 0) {
+    // take the first coordinates for the start and remove it from the array
     const start = endPoints.shift();
+    // iterate through each remaining end point, changing all 0's  on the path to 2's
     endPoints.forEach((end) => {
+      // dettermine the horizontal and vertical distances between start and end.
       const rise = start.row - end.row;
       const run = start.col - end.col;
+      // change all 0's to 2's
       if (movement) {
         createHorizontalPath(newChunk, rise, run, end, start);
       } else {
--- client/src/utilities/map/functions/create-blank-chunk.js
+++ client/src/utilities/map/functions/create-blank-chunk.js
@@ -2,15 +2,25 @@ import { applyRange } from "../../general/functions/utilityFunctions";
 // create a blank map chunk according to grid size
 const createBlankChunk = (gridSize) => {
   // make a 2D array of length gridSize
+  // For example, if gridSize is 2, newChunk will be
+  // [
+  // [0,0],
+  // [0,0]
+  // ]
   const newChunk = [];
   for (let i = 0; i < gridSize; i++) {
     const row = Array.from({ length: gridSize }, () => 0);
     newChunk.push(row);
   }
-  // randomly generate points in 2d array that will be used to create open
-  // spaces
+  // Randomly generate points in 2d array that will be used to create open spaces.
+  // In other words, each row will have one 0 get changed to a 2.
+  // We do this by pushing coordinates to endPoints, which will be returned with the actual
+  // map chunk.
   const endPoints = [];
-  // require first and last columns to have an endpoint to connect to other map chunks
+  // Require first and last columns to have an endpoint to connect to other map chunks.
+  // In other words, the leftmost and right most columns will have at least one 2.
+  // firstColRow and lastColRow are randomly assigned rows where either the first or last
+  // value will be changed to 2
   const firstColRow = applyRange(0, gridSize);
   const lastColRow = applyRange(0, gridSize);
   for (let n = 0; n < gridSize; n++) {
@@ -24,11 +34,13 @@ const createBlankChunk = (gridSize) => {
     }
     endPoints.push(randomColIndex);
   }
-  //write endpoints to the map chunk
+  // Change the 0 to a 2 at each endPoints coords
   for (const point of endPoints) {
     const { row, col } = point;
     newChunk[row][col] = 2;
   }
+  // map engine needs both newChunk because it's the actual map, 
+  // while endpoints will be used in the map creation algorithms.
   return { newChunk, endPoints };
 };
 
